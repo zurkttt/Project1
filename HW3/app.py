1f23ac0375103ad866f680612c8dc59bcdd0bc82
Kurt <thristankurt15@gmail.com>
2025-10-14 09:34:19 +0800

    Registration

---
+++ HW3/app.py
@@ -0,0 +1,28 @@
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+@app.route('/input')
+def input():
+    return render_template('index.html')  # your registration form
+
+@app.route('/process', methods=['POST'])
+def output():
+    last_name = request.form['lastName']
+    first_name = request.form['firstName']
+    sex = request.form['sex']
+    institution = request.form['Institution']
+    email = request.form['email']
+
+    # You can pass the values to result.html
+    return render_template(
+        'result.html',
+        last_name=last_name,
+        first_name=first_name,
+        sex=sex,
+        institution=institution,
+        email=email
+    )
+
+if __name__ == '__main__':
+    app.run(debug=True)
